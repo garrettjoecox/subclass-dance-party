adef34964caa1135703597243425d949424ccbe7
garrettjoecox <garrettjcox@gmail.com>
2015-02-09 16:05:39 -0800

    Complete a few of the basic requirements.

---
+++ src/bouncyDancer.js
@@ -0,0 +1,13 @@
+var BouncyDancer = function(top, left, timeBetweenSteps){
+  Dancer.apply(this, arguments);
+  this.setPosition(top, left);
+  this.clock(timeBetweenSteps);
+};
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+
+BouncyDancer.prototype.constructor = BouncyDancer;
+
+BouncyDancer.prototype.step = function(){
+  this.$node.addClass("bouncer")
+  this.$node.animate({"top": "-=100" }).delay(100).animate({"top": "+=100"});
+};
